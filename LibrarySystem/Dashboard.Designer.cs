0a226ce4a13ee3b1787824b3f8e2c4cd7eab0f18
ejay <ejaybuscato033@gmail.com>
2023-12-11 17:53:11 +0800

    finalized upperpanel

--- LibrarySystem/Dashboard.Designer.cs
+++ LibrarySystem/Dashboard.Designer.cs
@@ -47,6 +47,7 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(12, 22);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(48, 45);
