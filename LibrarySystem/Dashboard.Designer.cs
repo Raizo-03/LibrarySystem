130803723f0ccd05566e29bbc5a0e808b69d060b
ejay <ejaybuscato033@gmail.com>
2023-12-12 02:41:11 +0800

    finalized panel buttons

--- LibrarySystem/Dashboard.Designer.cs
+++ LibrarySystem/Dashboard.Designer.cs
@@ -33,18 +33,31 @@
             this.dashboardControlbx = new ReaLTaiizor.Controls.NightControlBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dashboardFlowPnl = new System.Windows.Forms.FlowLayoutPanel();
-            this.dashboardBtn2 = new System.Windows.Forms.Button();
             this.dashboardBtn = new System.Windows.Forms.Panel();
-            this.mainmenuFlowpnl = new System.Windows.Forms.FlowLayoutPanel();
             this.aboutBtn2 = new System.Windows.Forms.Panel();
             this.aboutBtn = new System.Windows.Forms.Button();
             this.exitBtn2 = new System.Windows.Forms.Panel();
             this.exitBtn = new System.Windows.Forms.Button();
+            this.dashboardBtn2 = new System.Windows.Forms.Button();
+            this.dashboardbackPnl = new System.Windows.Forms.FlowLayoutPanel();
+            this.booklistBtn2 = new System.Windows.Forms.Panel();
+            this.booklistBtn = new System.Windows.Forms.Button();
+            this.borrowBtn2 = new System.Windows.Forms.Panel();
+            this.borrowBtn = new System.Windows.Forms.Button();
+            this.returnBtn2 = new System.Windows.Forms.Panel();
+            this.returnBtn = new System.Windows.Forms.Button();
+            this.reserveBtn2 = new System.Windows.Forms.Panel();
+            this.reserveBtn = new System.Windows.Forms.Button();
             this.upperPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.dashboardBtn.SuspendLayout();
             this.aboutBtn2.SuspendLayout();
             this.exitBtn2.SuspendLayout();
+            this.dashboardbackPnl.SuspendLayout();
+            this.booklistBtn2.SuspendLayout();
+            this.borrowBtn2.SuspendLayout();
+            this.returnBtn2.SuspendLayout();
+            this.reserveBtn2.SuspendLayout();
             this.SuspendLayout();
             // 
             // upperPanel
@@ -97,43 +110,21 @@
             this.dashboardFlowPnl.Dock = System.Windows.Forms.DockStyle.Left;
             this.dashboardFlowPnl.Location = new System.Drawing.Point(0, 113);
             this.dashboardFlowPnl.Name = "dashboardFlowPnl";
-            this.dashboardFlowPnl.Size = new System.Drawing.Size(263, 487);
+            this.dashboardFlowPnl.Size = new System.Drawing.Size(274, 487);
             this.dashboardFlowPnl.TabIndex = 2;
-            // 
-            // dashboardBtn2
-            // 
-            this.dashboardBtn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dashboardBtn2.Image = ((System.Drawing.Image)(resources.GetObject("dashboardBtn2.Image")));
-            this.dashboardBtn2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.dashboardBtn2.Location = new System.Drawing.Point(-8, -11);
-            this.dashboardBtn2.Name = "dashboardBtn2";
-            this.dashboardBtn2.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
-            this.dashboardBtn2.Size = new System.Drawing.Size(294, 95);
-            this.dashboardBtn2.TabIndex = 3;
-            this.dashboardBtn2.Text = "      Dashboard";
-            this.dashboardBtn2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.dashboardBtn2.UseVisualStyleBackColor = true;
-            this.dashboardBtn2.Click += new System.EventHandler(this.button1_Click);
             // 
             // dashboardBtn
             // 
             this.dashboardBtn.Controls.Add(this.dashboardBtn2);
-            this.dashboardBtn.Location = new System.Drawing.Point(422, 298);
+            this.dashboardBtn.Location = new System.Drawing.Point(3, 3);
             this.dashboardBtn.Name = "dashboardBtn";
             this.dashboardBtn.Size = new System.Drawing.Size(275, 66);
             this.dashboardBtn.TabIndex = 4;
             // 
-            // mainmenuFlowpnl
-            // 
-            this.mainmenuFlowpnl.Location = new System.Drawing.Point(437, 172);
-            this.mainmenuFlowpnl.Name = "mainmenuFlowpnl";
-            this.mainmenuFlowpnl.Size = new System.Drawing.Size(260, 109);
-            this.mainmenuFlowpnl.TabIndex = 6;
-            // 
             // aboutBtn2
             // 
             this.aboutBtn2.Controls.Add(this.aboutBtn);
-            this.aboutBtn2.Location = new System.Drawing.Point(422, 388);
+            this.aboutBtn2.Location = new System.Drawing.Point(756, 400);
             this.aboutBtn2.Name = "aboutBtn2";
             this.aboutBtn2.Size = new System.Drawing.Size(275, 66);
             this.aboutBtn2.TabIndex = 5;
@@ -155,7 +146,7 @@
             // exitBtn2
             // 
             this.exitBtn2.Controls.Add(this.exitBtn);
-            this.exitBtn2.Location = new System.Drawing.Point(422, 478);
+            this.exitBtn2.Location = new System.Drawing.Point(756, 490);
             this.exitBtn2.Name = "exitBtn2";
             this.exitBtn2.Size = new System.Drawing.Size(275, 66);
             this.exitBtn2.TabIndex = 6;
@@ -174,16 +165,132 @@
             this.exitBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.exitBtn.UseVisualStyleBackColor = true;
             // 
+            // dashboardBtn2
+            // 
+            this.dashboardBtn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboardBtn2.Image = ((System.Drawing.Image)(resources.GetObject("dashboardBtn2.Image")));
+            this.dashboardBtn2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.dashboardBtn2.Location = new System.Drawing.Point(-8, -11);
+            this.dashboardBtn2.Name = "dashboardBtn2";
+            this.dashboardBtn2.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.dashboardBtn2.Size = new System.Drawing.Size(294, 95);
+            this.dashboardBtn2.TabIndex = 3;
+            this.dashboardBtn2.Text = "      Dashboard";
+            this.dashboardBtn2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.dashboardBtn2.UseVisualStyleBackColor = true;
+            this.dashboardBtn2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dashboardbackPnl
+            // 
+            this.dashboardbackPnl.Controls.Add(this.dashboardBtn);
+            this.dashboardbackPnl.Location = new System.Drawing.Point(422, 171);
+            this.dashboardbackPnl.Margin = new System.Windows.Forms.Padding(0);
+            this.dashboardbackPnl.Name = "dashboardbackPnl";
+            this.dashboardbackPnl.Size = new System.Drawing.Size(275, 363);
+            this.dashboardbackPnl.TabIndex = 7;
+            // 
+            // booklistBtn2
+            // 
+            this.booklistBtn2.Controls.Add(this.booklistBtn);
+            this.booklistBtn2.Location = new System.Drawing.Point(422, 246);
+            this.booklistBtn2.Margin = new System.Windows.Forms.Padding(0);
+            this.booklistBtn2.Name = "booklistBtn2";
+            this.booklistBtn2.Size = new System.Drawing.Size(275, 66);
+            this.booklistBtn2.TabIndex = 5;
+            // 
+            // booklistBtn
+            // 
+            this.booklistBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.booklistBtn.Image = ((System.Drawing.Image)(resources.GetObject("booklistBtn.Image")));
+            this.booklistBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.booklistBtn.Location = new System.Drawing.Point(-8, -11);
+            this.booklistBtn.Name = "booklistBtn";
+            this.booklistBtn.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.booklistBtn.Size = new System.Drawing.Size(294, 95);
+            this.booklistBtn.TabIndex = 3;
+            this.booklistBtn.Text = "      Book List";
+            this.booklistBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.booklistBtn.UseVisualStyleBackColor = true;
+            // 
+            // borrowBtn2
+            // 
+            this.borrowBtn2.Controls.Add(this.borrowBtn);
+            this.borrowBtn2.Location = new System.Drawing.Point(422, 317);
+            this.borrowBtn2.Name = "borrowBtn2";
+            this.borrowBtn2.Size = new System.Drawing.Size(275, 66);
+            this.borrowBtn2.TabIndex = 6;
+            // 
+            // borrowBtn
+            // 
+            this.borrowBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.borrowBtn.Image = ((System.Drawing.Image)(resources.GetObject("borrowBtn.Image")));
+            this.borrowBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.borrowBtn.Location = new System.Drawing.Point(-8, -11);
+            this.borrowBtn.Name = "borrowBtn";
+            this.borrowBtn.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.borrowBtn.Size = new System.Drawing.Size(294, 95);
+            this.borrowBtn.TabIndex = 3;
+            this.borrowBtn.Text = "      Borrow";
+            this.borrowBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.borrowBtn.UseVisualStyleBackColor = true;
+            // 
+            // returnBtn2
+            // 
+            this.returnBtn2.Controls.Add(this.returnBtn);
+            this.returnBtn2.Location = new System.Drawing.Point(422, 389);
+            this.returnBtn2.Name = "returnBtn2";
+            this.returnBtn2.Size = new System.Drawing.Size(275, 66);
+            this.returnBtn2.TabIndex = 7;
+            // 
+            // returnBtn
+            // 
+            this.returnBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.returnBtn.Image = ((System.Drawing.Image)(resources.GetObject("returnBtn.Image")));
+            this.returnBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.returnBtn.Location = new System.Drawing.Point(-8, -11);
+            this.returnBtn.Name = "returnBtn";
+            this.returnBtn.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.returnBtn.Size = new System.Drawing.Size(294, 95);
+            this.returnBtn.TabIndex = 3;
+            this.returnBtn.Text = "      Return";
+            this.returnBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.returnBtn.UseVisualStyleBackColor = true;
+            // 
+            // reserveBtn2
+            // 
+            this.reserveBtn2.Controls.Add(this.reserveBtn);
+            this.reserveBtn2.Location = new System.Drawing.Point(422, 461);
+            this.reserveBtn2.Name = "reserveBtn2";
+            this.reserveBtn2.Size = new System.Drawing.Size(275, 66);
+            this.reserveBtn2.TabIndex = 8;
+            // 
+            // reserveBtn
+            // 
+            this.reserveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reserveBtn.Image = ((System.Drawing.Image)(resources.GetObject("reserveBtn.Image")));
+            this.reserveBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.reserveBtn.Location = new System.Drawing.Point(-8, -11);
+            this.reserveBtn.Name = "reserveBtn";
+            this.reserveBtn.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.reserveBtn.Size = new System.Drawing.Size(294, 95);
+            this.reserveBtn.TabIndex = 3;
+            this.reserveBtn.Text = "      Reserve";
+            this.reserveBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.reserveBtn.UseVisualStyleBackColor = true;
+            // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 600);
             this.ControlBox = false;
+            this.Controls.Add(this.reserveBtn2);
+            this.Controls.Add(this.returnBtn2);
+            this.Controls.Add(this.borrowBtn2);
+            this.Controls.Add(this.booklistBtn2);
+            this.Controls.Add(this.dashboardbackPnl);
             this.Controls.Add(this.exitBtn2);
             this.Controls.Add(this.aboutBtn2);
-            this.Controls.Add(this.mainmenuFlowpnl);
-            this.Controls.Add(this.dashboardBtn);
             this.Controls.Add(this.dashboardFlowPnl);
             this.Controls.Add(this.upperPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -198,6 +305,11 @@
             this.dashboardBtn.ResumeLayout(false);
             this.aboutBtn2.ResumeLayout(false);
             this.exitBtn2.ResumeLayout(false);
+            this.dashboardbackPnl.ResumeLayout(false);
+            this.booklistBtn2.ResumeLayout(false);
+            this.borrowBtn2.ResumeLayout(false);
+            this.returnBtn2.ResumeLayout(false);
+            this.reserveBtn2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -207,12 +319,20 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private ReaLTaiizor.Controls.NightControlBox dashboardControlbx;
         private System.Windows.Forms.FlowLayoutPanel dashboardFlowPnl;
-        private System.Windows.Forms.Button dashboardBtn2;
         private System.Windows.Forms.Panel dashboardBtn;
-        private System.Windows.Forms.FlowLayoutPanel mainmenuFlowpnl;
         private System.Windows.Forms.Panel aboutBtn2;
         private System.Windows.Forms.Button aboutBtn;
         private System.Windows.Forms.Panel exitBtn2;
         private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button dashboardBtn2;
+        private System.Windows.Forms.FlowLayoutPanel dashboardbackPnl;
+        private System.Windows.Forms.Panel booklistBtn2;
+        private System.Windows.Forms.Button booklistBtn;
+        private System.Windows.Forms.Panel borrowBtn2;
+        private System.Windows.Forms.Button borrowBtn;
+        private System.Windows.Forms.Panel returnBtn2;
+        private System.Windows.Forms.Button returnBtn;
+        private System.Windows.Forms.Panel reserveBtn2;
+        private System.Windows.Forms.Button reserveBtn;
     }
 }
