6c24cb06e82cb1ac23362e467ef0ed5c942f401b
ejay <ejaybuscato033@gmail.com>
2023-12-13 08:04:31 +0800

    added function for log in

--- LibrarySystem/BorrowerList.cs
+++ LibrarySystem/BorrowerList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace LibrarySystem
 {
     public partial class BorrowerList : Form
     {
+        private List<User> users;  // Assuming User, Student, and Teacher classes are defined as in the previous example
+
         public BorrowerList()
         {
             InitializeComponent();
+
+
         }
 
         private void BorrowerList_Load(object sender, EventArgs e)
@@ -22,5 +28,26 @@ namespace LibrarySystem
             this.BackColor = Color.FromArgb(255, 253, 247, 228); //CUSTOM BG COLORS #FDF7E4
 
         }
+
+        //DISPLAYS THE INFO: 
+        /*
+        private void DisplayUserInformation()
+        {
+            // Assuming you have a ListBox named userListListBox in your form
+            userListListBox.Items.Clear();
+
+            foreach (var user in users)
+            {
+                if (user is Student student)
+                {
+                    userListListBox.Items.Add($"Student: {student.Name}, ID: {student.UserId}, Year: {student.YearLevel}, Section: {student.Section}");
+                }
+                else if (user is Teacher teacher)
+                {
+                    userListListBox.Items.Add($"Teacher: {teacher.Name}, ID: {teacher.UserId}, Employee ID: {teacher.EmployeeId}, Department: {teacher.Department}");
+                }
+            }
+        }
+        */
     }
 }
