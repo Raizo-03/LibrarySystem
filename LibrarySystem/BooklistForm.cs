7051c592c0809e1b5dc86c8f3d0849015c230767
ejay <ejaybuscato033@gmail.com>
2023-12-18 01:09:10 +0800

    edited booklist form

--- LibrarySystem/BooklistForm.cs
+++ LibrarySystem/BooklistForm.cs
@@ -7,154 +7,112 @@ namespace LibrarySystem
 {
     public partial class BooklistForm : Form
     {
-        private List<Book> bookList = new List<Book>();
-        private BindingSource bindingSource = new BindingSource();
+        private List<Book> books;
 
         public BooklistForm()
         {
             InitializeComponent();
-            InitializeDataGridView();
+            this.BackColor = Color.FromArgb(255, 253, 247, 228); // CUSTOM BG COLOR #FDF7E4
+            books = new List<Book>();
+
+            // Set DataGridView to read-only
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.CellSelect;
+
+            // Initialize DataGridView columns
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Title";
+            dataGridView1.Columns[1].Name = "Genre";
+            dataGridView1.Columns[2].Name = "Availability";
+
+            // Add DataGridViewComboBoxColumn for Genre
+            DataGridViewComboBoxColumn genreColumn = new DataGridViewComboBoxColumn();
+            genreColumn.HeaderText = "Genre";
+            genreColumn.Name = "Genre";
+            genreColumn.Items.AddRange("Fiction", "Non-Fiction", "Academic");
+            dataGridView1.Columns.Add(genreColumn);
+
+            // Add DataGridViewCheckBoxColumn for Availability
+            DataGridViewCheckBoxColumn availabilityColumn = new DataGridViewCheckBoxColumn();
+            availabilityColumn.HeaderText = "Availability";
+            availabilityColumn.Name = "Availability";
+            dataGridView1.Columns.Add(availabilityColumn);
+
+            // Attach the SortCompare event handler for Genre column sorting
+            dataGridView1.SortCompare += DataGridView1_SortCompare;
+        }
+
+        // Add a method to populate the DataGridView with books
+        private void PopulateDataGridView()
+        {
+            dataGridView1.Rows.Clear(); // Clear existing rows
+
+            foreach (var book in books)
+            {
+                int rowIndex = dataGridView1.Rows.Add();
+                dataGridView1.Rows[rowIndex].Cells["Title"].Value = book.Title;
+                dataGridView1.Rows[rowIndex].Cells["Genre"].Value = book.Genre;
+                dataGridView1.Rows[rowIndex].Cells["Availability"].Value = book.IsAvailable;
+            }
         }
 
         private void BooklistForm_Load(object sender, EventArgs e)
         {
-            // Perform any additional initialization here if needed
+            // Optional: You can load additional data or perform any setup when the form loads.
         }
 
-        private void InitializeDataGridView()
+        private void fictionButton_Click(object sender, EventArgs e)
         {
-            bookTable.DefaultCellStyle.Font = new Font("Arial", 12, FontStyle.Regular);
-            bookTable.DefaultCellStyle.SelectionBackColor = Color.Black;
-
-            // Customize DataGridView properties as needed
-            bookTable.AutoGenerateColumns = false;
-
-
-            // Set DataPropertyName for each column
-            DataGridViewTextBoxColumn titleColumn = new DataGridViewTextBoxColumn();
-            titleColumn.Name = "Title";
-            titleColumn.HeaderText = "Title";
-            titleColumn.DataPropertyName = "Title";
-            //titleColumn.DefaultCellStyle.Font = new Font("Arial", 12, FontStyle.Bold); // Set font style
-            bookTable.Columns.Add(titleColumn);
-
-          
-
-            // ISBN Column
-            DataGridViewTextBoxColumn isbnColumn = new DataGridViewTextBoxColumn();
-            isbnColumn.Name = "ISBN";
-            isbnColumn.HeaderText = "ISBN";
-            isbnColumn.DataPropertyName = "ISBN";
-            bookTable.Columns.Add(isbnColumn);
-
-            // Category Column
-            DataGridViewTextBoxColumn categoryColumn = new DataGridViewTextBoxColumn();
-            categoryColumn.Name = "Category";
-            categoryColumn.HeaderText = "Category";
-            categoryColumn.DataPropertyName = "Category";
-            bookTable.Columns.Add(categoryColumn);
-
-            // Author Column
-            DataGridViewTextBoxColumn authorColumn = new DataGridViewTextBoxColumn();
-            authorColumn.Name = "Author";
-            authorColumn.HeaderText = "Author";
-            authorColumn.DataPropertyName = "Author";
-            bookTable.Columns.Add(authorColumn);
-
-            // Copyright Column
-            DataGridViewTextBoxColumn copyrightColumn = new DataGridViewTextBoxColumn();
-            copyrightColumn.Name = "Copyright";
-            copyrightColumn.HeaderText = "Copyright";
-            copyrightColumn.DataPropertyName = "Copyright";
-            bookTable.Columns.Add(copyrightColumn);
-
-            // Publisher Column
-            DataGridViewTextBoxColumn publisherColumn = new DataGridViewTextBoxColumn();
-            publisherColumn.Name = "Publisher";
-            publisherColumn.HeaderText = "Publisher";
-            publisherColumn.DataPropertyName = "Publisher";
-            bookTable.Columns.Add(publisherColumn);
-
-            // Status Column
-            DataGridViewTextBoxColumn statusColumn = new DataGridViewTextBoxColumn();
-            statusColumn.Name = "Status";
-            statusColumn.HeaderText = "Status";
-            statusColumn.DataPropertyName = "Status";
-            bookTable.Columns.Add(statusColumn);
-
-            bookTable.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-
-            // Set the default selection back color to white
-            bookTable.DefaultCellStyle.SelectionBackColor = Color.Transparent;
-
-            bookTable.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.Transparent;
-
-            bindingSource.DataSource = bookList;
-            bookTable.DataSource = bindingSource;
-
-            // Handle the RowStateChanged event to change the background color of the selected row
-            bookTable.RowStateChanged += BookTableRowStateChanged;
-
-            bookTable.CellFormatting += BookTableCellFormatting;
-
-        }
-
-        private void BookTableRowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
-        {
-            
-        }
-        private void BookTableCellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
-        {
-            // Check if the cell is not a header cell
-            if (e.RowIndex >= 0)
-            {
-                // Set the font color for each cell in the row to black
-                e.CellStyle.ForeColor = Color.Black;
-            }
-        }
-
-        public class Book
-        {
-            public System.Drawing.Image Image { get; set; }
-            public string Title { get; set; }
-            public string ISBN { get; set; }
-            public string Category { get; set; }
-            public string Author { get; set; }
-            public int Copyright { get; set; }
-            public string Publisher { get; set; }
-            public string Status { get; set; }
+            // Optional: You can implement logic for handling the fictionButton click event.
         }
 
         private void rjButton1_Click(object sender, EventArgs e)
         {
-            // Set the Image for the new book
-            System.Drawing.Image bookImage = System.Drawing.Image.FromFile(@"C:\Users\USER\source\repos\LibrarySystem\images\aboutIcon.png");
+            // Optional: You can implement logic for handling the rjButton1 click event.
+        }
 
-            // Add a predefined book to the list
-            Book newBook = new Book
+        private void ShowBookDetails(Book book)
+        {
+            // Optional: You can implement logic for displaying book details.
+        }
+
+        private void availBtn_Click(object sender, EventArgs e)
+        {
+            // Add predefined books
+            books.Add(new Book { Title = "To Kill a Mockingbird", Genre = "Fiction", IsAvailable = true });
+            books.Add(new Book { Title = "1984", Genre = "Fiction", IsAvailable = true });
+            books.Add(new Book { Title = "Pride and Prejudice", Genre = "Fiction", IsAvailable = true });
+            books.Add(new Book { Title = "The Great Gatsby", Genre = "Fiction", IsAvailable = true });
+            books.Add(new Book { Title = "One Hundred Years of Solitude", Genre = "Fiction", IsAvailable = true });
+            books.Add(new Book { Title = "Sapiens: A Brief History of Humankind", Genre = "Non-Fiction", IsAvailable = true });
+            books.Add(new Book { Title = "The Immortal Life of Henrietta Lacks", Genre = "Non-Fiction", IsAvailable = true });
+            books.Add(new Book { Title = "Educated", Genre = "Non-Fiction", IsAvailable = true });
+            books.Add(new Book { Title = "The Diary of a Young Girl", Genre = "Non-Fiction", IsAvailable = true });
+            books.Add(new Book { Title = "Thinking, Fast and Slow", Genre = "Non-Fiction", IsAvailable = true });
+            books.Add(new Book { Title = "A Brief History of Time", Genre = "Academic", IsAvailable = true });
+            books.Add(new Book { Title = "The Elements of Style", Genre = "Academic", IsAvailable = true });
+            books.Add(new Book { Title = "Principia Mathematica", Genre = "Academic", IsAvailable = true });
+            books.Add(new Book { Title = "The Structure of Scientific Revolutions", Genre = "Academic", IsAvailable = true });
+            books.Add(new Book { Title = "The Origin of Species", Genre = "Academic", IsAvailable = true });
+
+            // Populate the DataGridView with the updated list of books
+            PopulateDataGridView();
+        }
+        // Event handler for the SortCompare event to customize sorting for Genre column
+        private void DataGridView1_SortCompare(object sender, DataGridViewSortCompareEventArgs e)
+        {
+            if (e.Column.Name == "Genre") // Check if sorting is on the Genre column
             {
-                Image = bookImage,
-                Title = "Sample Title",
-                ISBN = "1234567890",
-                Category = "Fiction",
-                Author = "John Doe",
-                Copyright = 2023,
-                Publisher = "Sample Publisher",
-                Status = "Available"
-            };
-            // Add the new book to the list
-            bookList.Add(newBook);
-
-            // Update the BindingSource to refresh the DataGridView
-            bindingSource.ResetBindings(false);
-
-            // Select the last row (newly added book)
-            if (bookTable.Rows.Count > 0)
-            {
-                bookTable.Rows[bookTable.Rows.Count -1].Selected = true;
+                e.SortResult = String.Compare(e.CellValue1.ToString(), e.CellValue2.ToString());
+                e.Handled = true; // Handle the sorting
             }
-
-
+        }
+        
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Genre { get; set; }
+            public bool IsAvailable { get; set; }
         }
     }
 }
