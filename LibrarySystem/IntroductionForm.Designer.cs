549471643d9a0479b9d0f0678fde2adb485c3f56
Raizo-03 <123054277+Raizo-03@users.noreply.github.com>
2023-12-17 11:55:55 +0800

    added icon to the login (#44)

--- LibrarySystem/IntroductionForm.Designer.cs
+++ LibrarySystem/IntroductionForm.Designer.cs
@@ -32,11 +32,15 @@
             this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
             this.nightControlBox1 = new ReaLTaiizor.Controls.NightControlBox();
             this.EternalLibPic = new System.Windows.Forms.Label();
-            this.proceedBtn = new LibrarySystem.RJButton();
             this.usernameBx = new System.Windows.Forms.TextBox();
             this.passwordBx = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.lblMessage = new System.Windows.Forms.Label();
+            this.userIcon = new System.Windows.Forms.PictureBox();
+            this.passIcon = new System.Windows.Forms.PictureBox();
+            this.proceedBtn = new LibrarySystem.RJButton();
+            ((System.ComponentModel.ISupportInitialize)(this.userIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passIcon)).BeginInit();
             this.SuspendLayout();
             // 
             // nightControlBox1
@@ -72,26 +76,6 @@
             this.EternalLibPic.Size = new System.Drawing.Size(225, 210);
             this.EternalLibPic.TabIndex = 1;
             // 
-            // proceedBtn
-            // 
-            this.proceedBtn.BackColor = System.Drawing.Color.White;
-            this.proceedBtn.BackgroundColor = System.Drawing.Color.White;
-            this.proceedBtn.BorderColor = System.Drawing.Color.Olive;
-            this.proceedBtn.BorderRadius = 29;
-            this.proceedBtn.BorderSize = 2;
-            this.proceedBtn.FlatAppearance.BorderSize = 0;
-            this.proceedBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.proceedBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.proceedBtn.ForeColor = System.Drawing.Color.Olive;
-            this.proceedBtn.Location = new System.Drawing.Point(89, 441);
-            this.proceedBtn.Name = "proceedBtn";
-            this.proceedBtn.Size = new System.Drawing.Size(224, 59);
-            this.proceedBtn.TabIndex = 3;
-            this.proceedBtn.Text = "PROCEED";
-            this.proceedBtn.TextColor = System.Drawing.Color.Olive;
-            this.proceedBtn.UseVisualStyleBackColor = false;
-            this.proceedBtn.Click += new System.EventHandler(this.proceedBtn_Click);
-            // 
             // usernameBx
             // 
             this.usernameBx.Location = new System.Drawing.Point(106, 338);
@@ -124,12 +108,52 @@
             this.lblMessage.TabIndex = 7;
             this.lblMessage.Text = "label2";
             // 
+            // userIcon
+            // 
+            this.userIcon.Image = ((System.Drawing.Image)(resources.GetObject("userIcon.Image")));
+            this.userIcon.Location = new System.Drawing.Point(69, 327);
+            this.userIcon.Name = "userIcon";
+            this.userIcon.Size = new System.Drawing.Size(31, 31);
+            this.userIcon.TabIndex = 8;
+            this.userIcon.TabStop = false;
+            // 
+            // passIcon
+            // 
+            this.passIcon.Image = ((System.Drawing.Image)(resources.GetObject("passIcon.Image")));
+            this.passIcon.Location = new System.Drawing.Point(69, 374);
+            this.passIcon.Name = "passIcon";
+            this.passIcon.Size = new System.Drawing.Size(31, 34);
+            this.passIcon.TabIndex = 9;
+            this.passIcon.TabStop = false;
+            // 
+            // proceedBtn
+            // 
+            this.proceedBtn.BackColor = System.Drawing.Color.White;
+            this.proceedBtn.BackgroundColor = System.Drawing.Color.White;
+            this.proceedBtn.BorderColor = System.Drawing.Color.Olive;
+            this.proceedBtn.BorderRadius = 29;
+            this.proceedBtn.BorderSize = 2;
+            this.proceedBtn.FlatAppearance.BorderSize = 0;
+            this.proceedBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.proceedBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.proceedBtn.ForeColor = System.Drawing.Color.Olive;
+            this.proceedBtn.Location = new System.Drawing.Point(89, 441);
+            this.proceedBtn.Name = "proceedBtn";
+            this.proceedBtn.Size = new System.Drawing.Size(224, 59);
+            this.proceedBtn.TabIndex = 3;
+            this.proceedBtn.Text = "PROCEED";
+            this.proceedBtn.TextColor = System.Drawing.Color.Olive;
+            this.proceedBtn.UseVisualStyleBackColor = false;
+            this.proceedBtn.Click += new System.EventHandler(this.proceedBtn_Click);
+            // 
             // FirstForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(392, 576);
             this.ControlBox = false;
+            this.Controls.Add(this.passIcon);
+            this.Controls.Add(this.userIcon);
             this.Controls.Add(this.lblMessage);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.passwordBx);
@@ -143,6 +167,8 @@
             this.Name = "FirstForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.FirstForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.userIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passIcon)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,6 +184,8 @@
         private System.Windows.Forms.TextBox passwordBx;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.PictureBox userIcon;
+        private System.Windows.Forms.PictureBox passIcon;
     }
 }
 
