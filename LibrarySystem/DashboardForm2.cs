988dbf41fbfded0d633215b74c991753c809d821
Raizo-03 <123054277+Raizo-03@users.noreply.github.com>
2023-12-29 13:41:05 +0800

    update function in dashboard form2; reserve button

--- LibrarySystem/DashboardForm2.cs
+++ LibrarySystem/DashboardForm2.cs
@@ -162,6 +162,58 @@ namespace LibrarySystem
             }
         }
 
+        private void borrowedB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Clear existing columns in the DataGridView
+                analyticsDG.Columns.Clear();
+
+                // Fetch borrowing information from the database
+                List<BorrowingInfo> borrowings = GetBorrowingsInfo();
+
+                // Add columns to the DataGridView
+                analyticsDG.Columns.Add("BookTitle", "BOOK TITLE");
+                analyticsDG.Columns.Add("BorrowerName", "BORROWER'S NAME");
+                analyticsDG.Columns.Add("BorrowDate", "BORROW DATE");
+                analyticsDG.Columns.Add("DueDate", "DUE DATE");
+
+                // Set font size and apply modern style
+                analyticsDG.DefaultCellStyle.Font = new Font("Segoe UI", 10); // Adjust font and size
+                analyticsDG.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 12, FontStyle.Bold); // Adjust font, size, and style
+                analyticsDG.EnableHeadersVisualStyles = false;
+                analyticsDG.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(37, 37, 38); // Header background color
+                analyticsDG.ColumnHeadersDefaultCellStyle.ForeColor = Color.White; // Header text color
+
+                // Set column widths
+                analyticsDG.Columns["BookTitle"].Width = 200; // Adjust width as needed
+                analyticsDG.Columns["BorrowerName"].Width = 200; // Adjust width as needed
+                analyticsDG.Columns["BorrowDate"].Width = 150; // Adjust width as needed
+                analyticsDG.Columns["DueDate"].Width = 150; // Adjust width as needed
+
+                // Disable user resizing of rows and columns
+                analyticsDG.AllowUserToResizeRows = false;
+                analyticsDG.AllowUserToResizeColumns = false;
+
+                // Disable row headers resizing and visibility
+                analyticsDG.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+
+                // Bind the list to the DataGridView
+                foreach (BorrowingInfo borrowing in borrowings)
+                {
+                    int rowIndex = analyticsDG.Rows.Add();
+                    analyticsDG.Rows[rowIndex].Cells["BookTitle"].Value = borrowing.BookTitle;
+                    analyticsDG.Rows[rowIndex].Cells["BorrowerName"].Value = borrowing.BorrowerName;
+                    analyticsDG.Rows[rowIndex].Cells["BorrowDate"].Value = borrowing.BorrowDate;
+                    analyticsDG.Rows[rowIndex].Cells["DueDate"].Value = borrowing.DueDate;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
         private void DashboardForm2_Load(object sender, EventArgs e)
         {
             this.BackColor = Color.FromArgb(255, 253, 247, 228); // CUSTOM BG COLORS #FDF7E4
@@ -186,7 +238,54 @@ namespace LibrarySystem
 
         }
 
-       
+        private void reservedB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Clear existing columns in the DataGridView
+                analyticsDG.Columns.Clear();
+
+                // Fetch reservation information from the database
+                List<ReservationInfo> reservations = GetReservationsInfo();
+
+                // Add columns to the DataGridView
+                analyticsDG.Columns.Add("BookTitle", "BOOK TITLE");
+                analyticsDG.Columns.Add("BorrowerName", "BORROWER'S NAME");
+                analyticsDG.Columns.Add("ReservationDate", "RESERVATION DATE");
+
+                // Set font size and apply modern style
+                analyticsDG.DefaultCellStyle.Font = new Font("Segoe UI", 10); // Adjust font and size
+                analyticsDG.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 12, FontStyle.Bold); // Adjust font, size, and style
+                analyticsDG.EnableHeadersVisualStyles = false;
+                analyticsDG.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(37, 37, 38); // Header background color
+                analyticsDG.ColumnHeadersDefaultCellStyle.ForeColor = Color.White; // Header text color
+
+                // Set column widths
+                analyticsDG.Columns["BookTitle"].Width = 200; // Adjust width as needed
+                analyticsDG.Columns["BorrowerName"].Width = 200; // Adjust width as needed
+                analyticsDG.Columns["ReservationDate"].Width = 150; // Adjust width as needed
+
+                // Disable user resizing of rows and columns
+                analyticsDG.AllowUserToResizeRows = false;
+                analyticsDG.AllowUserToResizeColumns = false;
+
+                // Disable row headers resizing and visibility
+                analyticsDG.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+
+                // Bind the list to the DataGridView
+                foreach (ReservationInfo reservation in reservations)
+                {
+                    int rowIndex = analyticsDG.Rows.Add();
+                    analyticsDG.Rows[rowIndex].Cells["BookTitle"].Value = reservation.BookTitle;
+                    analyticsDG.Rows[rowIndex].Cells["BorrowerName"].Value = reservation.BorrowerName;
+                    analyticsDG.Rows[rowIndex].Cells["ReservationDate"].Value = reservation.ReservationDate;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
 
         private int GetTotalAvailableBooks()
         {
@@ -204,5 +303,109 @@ namespace LibrarySystem
                 }
             }
         }
+        private List<BorrowingInfo> GetBorrowingsInfo()
+        {
+            List<BorrowingInfo> borrowings = new List<BorrowingInfo>();
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // SQL query to get borrowing information with DATE_FORMAT to remove time
+                    string query = "SELECT borrowings.book_title AS BookTitle, borrowers.name AS BorrowerName, " +
+                                   "DATE_FORMAT(borrowings.borrow_date, '%Y-%m-%d') AS BorrowDate, " +
+                                   "DATE_FORMAT(borrowings.due_date, '%Y-%m-%d') AS DueDate " +
+                                   "FROM borrowings " +
+                                   "INNER JOIN borrowers ON borrowings.user_id = borrowers.user_id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Create a BorrowingInfo object and populate its properties
+                                BorrowingInfo borrowing = new BorrowingInfo
+                                {
+                                    BookTitle = reader["BookTitle"].ToString(),
+                                    BorrowerName = reader["BorrowerName"].ToString(),
+                                    BorrowDate = reader["BorrowDate"].ToString(),
+                                    DueDate = reader["DueDate"].ToString()
+                                };
+
+                                // Add the borrowing information to the list
+                                borrowings.Add(borrowing);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error fetching borrowing information: {ex.Message}");
+            }
+
+            return borrowings;
+        }
+        private List<ReservationInfo> GetReservationsInfo()
+        {
+            List<ReservationInfo> reservations = new List<ReservationInfo>();
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // SQL query to get reservation information with DATE_FORMAT to remove time
+                    string query = "SELECT reservations.book_id, books.title AS BookTitle, borrowers.name AS BorrowerName, " +
+                                   "DATE_FORMAT(reservations.reservation_date, '%Y-%m-%d') AS ReservationDate " +
+                                   "FROM reservations " +
+                                   "INNER JOIN books ON reservations.book_id = books.book_id " +
+                                   "INNER JOIN borrowers ON reservations.borrower_id = borrowers.user_id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Create a ReservationInfo object and populate its properties
+                                ReservationInfo reservation = new ReservationInfo
+                                {
+                                    BookTitle = reader["BookTitle"].ToString(),
+                                    BorrowerName = reader["BorrowerName"].ToString(),
+                                    ReservationDate = reader["ReservationDate"].ToString()
+                                };
+
+                                // Add the reservation information to the list
+                                reservations.Add(reservation);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error fetching reservation information: {ex.Message}");
+            }
+
+            return reservations;
+        }
+    }
+    public class BorrowingInfo
+    {
+        public string BookTitle { get; set; }
+        public string BorrowerName { get; set; }
+        public string BorrowDate { get; set; }
+        public string DueDate { get; set; }
+    }
+    public class ReservationInfo
+    {
+        public string BookTitle { get; set; }
+        public string BorrowerName { get; set; }
+        public string ReservationDate { get; set; }
     }
 }
